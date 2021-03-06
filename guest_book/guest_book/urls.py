5054a9aa17c6bf19e727c407ea91675622fcd134
Ruslan <rusik.kros@gmail.com>
2021-03-06 17:47:28 +0600

    Изменения в guest_book

--- guest_book/guest_book/urls.py
+++ guest_book/guest_book/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import add_recording, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recording/add/', add_recording),
+    path('', home)
 ]
