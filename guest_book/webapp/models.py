c65785015754f17310ac41915cd167e3badf2e2d
Ruslan <rusik.kros@gmail.com>
2021-03-06 13:52:47 +0600

    Добавление модели Гостевой книгии

--- guest_book/webapp/models.py
+++ guest_book/webapp/models.py
@@ -8,4 +8,4 @@ class Guest_book(models.Model):
     content = models.TextField(max_length=3000, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.Choices(activ, blocked)
+    status = models.TextField(max_length=7, null=False, blank=False)
