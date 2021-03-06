9df3a7d456cc0c3b23dab222f83addff96a4ed39
Ruslan <rusik.kros@gmail.com>
2021-03-06 13:32:05 +0600

    Добавление модели Гостевой книги

--- guest_book/webapp/models.py
+++ guest_book/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Guest_book(models.Model):
+    author = models.CharField(max_length=150, null=False, blank=False)
+    email = models.EmailField(max_length=150, null=False, blank=False)
+    content = models.TextField(max_length=3000, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.Choices(activ, blocked)
